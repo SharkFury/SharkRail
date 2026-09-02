7c2b9bf52320f2e484776f2bd29b1fce252adfb1
larrychen <619841516@qq.com>
2026-09-02 23:20:17 +0800

    chore: reorder imports in package initializer

--- src/sharkrail/__init__.py
+++ src/sharkrail/__init__.py
@@ -2,15 +2,15 @@
 
 __version__ = "0.0.1"
 
-from .models import CommandMode, CommandSpec
+from .capabilities import Capability, collect
 from .executor import (
     CommandResult,
     CommandRunner,
     LifecycleEvent,
     LifecycleEventType,
 )
-from .capabilities import Capability, collect
 from .lifecycle import InvalidTransition, SessionLifecycle, SessionState
+from .models import CommandMode, CommandSpec
 
 __all__ = [
     "CommandMode",
